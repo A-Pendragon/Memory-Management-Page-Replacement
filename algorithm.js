ad41a7a993ec6f1a1282fb0f6874f4e1244e8128
DevChrisCross <Dev.ChrisMolina@gmail.com>
2017-03-26 16:42:13 +0800

    page optimal algorithm added

--- algorithm.js
+++ algorithm.js
@@ -12,7 +12,7 @@
         { id: 2 },
         { id: 1 },
         { id: 3 }
-    ], 3, "fifo"));
+    ], 3, "optimal"));
 
     function pageReplacement(pageStream, frameLength, replacementType) {
         let frameHeader = 0;
@@ -22,13 +22,12 @@
         if(replacementType === 'fifo'){
             for(let pageHeader = 0; pageHeader < pageStream.length; pageHeader++){
                 let pageFault = true;
-                let isPageExisting = (
-                    frame.find(function (page) {
+                let isPageExisting = (frame.find(
+                    function (page) {
                         return page.id === pageStream[pageHeader].id;
                     })
                 ) !== undefined;
 ;
-
                 if(isPageExisting){
                     pageFault = false;
                 }else{
@@ -45,12 +44,50 @@
                     frame: duplicateObject(frame)
                 });
             }
-
-            return frameStream;
         }
 
         if(replacementType === "optimal"){
+            for(let pageHeader = 0; pageHeader < pageStream.length; pageHeader++){
+                let pageFault = true;
+                let isPageExisting = (
+                    frame.find(function (page) {
+                        return page.id === pageStream[pageHeader].id;
+                    })
+                ) !== undefined;
 
+                if(isPageExisting){
+                    pageFault = false;
+                }else{
+                    if(frameHeader < frameLength){
+                        frame[frameHeader] = pageStream[pageHeader];
+                        frameHeader++;
+                    }else {
+                        let pageStreamLookUp = pageStream.slice(pageHeader+1, pageStream.length);
+                        let pageOptimality = [];
+
+                        for(let frameCell = 0; frameCell < frame.length; frameCell++){
+                            let pageNextLocation = pageStreamLookUp.findIndex(function (page) {
+                                return page.id === pageStreamLookUp[frameCell].id;
+                            });
+                            pageOptimality.push(pageNextLocation);
+                        }
+
+                        let unusedPage = pageOptimality.find(function (index) {
+                            return index === -1;
+                        });
+
+                        let indexOfMaximum = unusedPage === undefined ?
+                            pageOptimality.indexOf(Math.max.apply(Math, pageOptimality)) : pageOptimality.indexOf(-1);
+
+                        frame[indexOfMaximum] = pageStream[pageHeader];
+                    }
+                }
+
+                frameStream.push({
+                    pageFault: pageFault,
+                    frame: duplicateObject(frame)
+                });
+            }
         }
 
         if(replacementType === "second chance"){
@@ -60,6 +97,8 @@
         if(replacementType === "lfu"){
 
         }
+
+        return frameStream;
     }
 
 
