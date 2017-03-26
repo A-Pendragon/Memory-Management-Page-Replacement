ec07175dad741a7a1cd910c438d2ab91ec5fabfd
DevChrisCross <Dev.ChrisMolina@gmail.com>
2017-03-26 15:22:36 +0800

    fifo page replacement completed

--- algorithm.js
+++ algorithm.js
@@ -1,35 +1,64 @@
 (function () {
     "use strict";
-    
+
+    console.log(pageReplacement([
+        { id: 1 },
+        { id: 2 },
+        { id: 1 },
+        { id: 3 },
+        { id: 1 },
+        { id: 2 },
+        { id: 4 },
+        { id: 2 },
+        { id: 1 },
+        { id: 3 }
+    ], 3, "fifo"));
+
     function pageReplacement(pageStream, frameLength, replacementType) {
         let frameHeader = 0;
         let frameStream = [];
         let frame = [];
 
-        for(let pageHeader = 0; pageHeader < pageStream.length; pageHeader++){
-            let pageFault = true;
-            let currentPage = pageStream[pageHeader];
-            let currentFrame = frame[frameHeader];
+        if(replacementType === 'fifo'){
+            for(let pageHeader = 0; pageHeader < pageStream.length; pageHeader++){
+                let pageFault = true;
+                let isPageExisting = (
+                    frame.find(function (page) {
+                        return page.id === pageStream[pageHeader].id;
+                    })
+                ) !== undefined;
+;
 
-            let isPageExisting = frame.find(function (page) {
-                return page.id === currentPage.id;
-            });
-
-            if(isPageExisting){
-                pageFault = false;
-            }else{
-                currentFrame = duplicateObject(currentPage);
-                if(frameHeader < frameLength){
-                    frameHeader++;
-                }else {
-                    frameHeader = 0;
+                if(isPageExisting){
+                    pageFault = false;
+                }else{
+                    frame[frameHeader] = pageStream[pageHeader];
+                    if(frameHeader+1 < frameLength){
+                        frameHeader++;
+                    }else {
+                        frameHeader = 0;
+                    }
                 }
+
+                frameStream.push({
+                    pageFault: pageFault,
+                    frame: duplicateObject(frame)
+                });
             }
 
-            frameStream.push({
-                pageFault: pageFault,
-                frame: duplicateObject(frame)
-            });
+            return frameStream;
+        }
+
+        if(replacementType === "optimal"){
+
+        }
+
+        if(replacementType === "second chance"){
+
+        }
+
+        if(replacementType === "lfu"){
+
         }
     }
 
